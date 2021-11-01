62c320f5290f5844f9d9302f26f862f2a072aff6
Thanos Stamatakis <thanos.stamatakis@gmail.com>
2021-11-02 00:16:27 +0200

    Initial Commit

---
+++ brightness-control/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  brightness-control
+//
+//  Created by Thanos Stamatakis on 2/11/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ brightness-control/brightness_controlApp.swift
@@ -0,0 +1,17 @@
+//
+//  brightness_controlApp.swift
+//  brightness-control
+//
+//  Created by Thanos Stamatakis on 2/11/21.
+//
+
+import SwiftUI
+
+@main
+struct brightness_controlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
